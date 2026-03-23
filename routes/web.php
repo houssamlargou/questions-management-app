ac5c2cd0b186a38ca3dc40a5e5c87790ac529e18
houssam largou <hossamlargo5@gmail.com>
2026-03-23 05:06:50 +0100

    QMA-22 create answer routes and controller #comment Added the answer controller and created the authenticated route for storing answer under questions #done

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\AnswerController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/questions', [QuestionController::class, 'index'])->middleware('auth')->name('questions.index');
+Route::get('/questions/create', [QuestionController::class, 'create'])->middleware('auth')->name('questions.create');
+Route::post('/questions',[QuestionController::class, 'store'])->middleware('auth')->name('question.store');
+Route::get('/questions/{id}', [QuestionController::class, 'show'])->name('questions.show');
+Route::post('/questions/{question}/answer', [AnswerController::class,'store'])->middleware('auth')->name('answers.store');
 Route::get('/login',[AuthController::class, 'showLogin'])->middleware('guest')->name('login');
 Route::get('/register', [AuthController::class, 'showRegister'])->middleware('guest')->name('register');
 Route::post('/register',[AuthController::class, 'register'])->middleware('guest');
