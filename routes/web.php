d5dd2235131d7509277696ed4e4fb519d326ad35
houssam largou <hossamlargo5@gmail.com>
2026-03-24 10:22:47 +0100

    QMA-34 create profile route and controller #comment Added the ProfileController and created the authenticated profile route #done

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\AnswerController;
 use App\Http\Controllers\FavoriteController;
+use App\Http\controllers\ProfileController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,6 +18,7 @@ Route::get('/questions/{id}', [QuestionController::class, 'show'])->name('questi
 Route::post('/questions/{question}/answer', [AnswerController::class,'store'])->middleware('auth')->name('answers.store');
 Route::post('/questions/{question}/favorite', [FavoriteController::class, 'store'])->middleware('auth')->name('favorites.store');
 Route::delete('/questions/{question}/favorite', [FavoriteController::class, 'destroy'])->middleware('auth')->name('favorites.destroy');
+Route::get('/profile', [ProfileController::class, 'index'])->middleware('auth')->name('profile.index');
 Route::get('/login',[AuthController::class, 'showLogin'])->middleware('guest')->name('login');
 Route::get('/register', [AuthController::class, 'showRegister'])->middleware('guest')->name('register');
 Route::post('/register',[AuthController::class, 'register'])->middleware('guest');
