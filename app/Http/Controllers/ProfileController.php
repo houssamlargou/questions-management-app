12e51d7e7f30996d9795bf7ac0f56ae7672c4c56
houssam largou <hossamlargo5@gmail.com>
2026-03-24 11:36:30 +0100

    QMA-37 show question and favorite counts on profile page #comment Loaded question and favorite counts for the authenticated user and displayed them on the profile page stats cards #done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -7,7 +7,15 @@ use Illuminate\Http\Request;
 class ProfileController extends Controller
 {
     public function index(Request $request){
-        $user = $request->user();
+        $user = $request->user()->loadCount(['questions', 'favorites']);
+        $user->load([
+            'questions' => function($query) {
+                $query->latest();
+            },
+            'favorites.question.user' => function ($query) {
+                $query->latest();
+            },
+        ]);
 
         return view('profile.index', compact('user'));
     }
